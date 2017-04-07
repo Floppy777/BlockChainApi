718cda165b493b6437a1be19ad913014c84cd150
fchambon <fchambon@objectif.paris>
2017-04-07 16:38:23 +0200

    Ajout du client elasticsearch

--- routes/wallet.js
+++ routes/wallet.js
@@ -2,21 +2,33 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var walletEthereum = require('ethereumjs-wallet');
 var keythereum = require("keythereum");
+var elasticsearch = require('elasticsearch');
 var fs = require('fs');
 var router = express.Router();
 router.use(bodyParser.json());
+
+var client = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log : 'trace'
+});
 
 router
 	.get('/create/:password',function(req,res){	
 		var password = req.params.password;
 		var wallet = walletEthereum.generate(true);
 		var v3File = wallet.toV3(password);
-		/*fs.writeFile("/tmp/test",v3File,function(err) {
-    	if(err) {
-        	return console.log(err);
-    	}
-		});*/
-		keythereum.exportToFile(v3File); 
+		var elasticKey = {};
+		elasticKey.id = v3File.id;
+		elasticKey.index = "ethereumwallets"
+		elasticKey.type = "wallet"
+		elasticKey.body = v3File;
+		client.create(elasticKey,function(error,response){
+			if(error){
+				console.log(error);
+			}
+			console.log(response);
+			keythereum.exportToFile(v3File); 
+		});
 		res.json(v3File);
 	});
 
